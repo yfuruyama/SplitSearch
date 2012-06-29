d5871313c7062b8771ce8a6a9b11ed41fb1e3b27
Yuuki Furuyama <addsict@gmail.com>
2012-06-30 01:46:59 +0900

    リンクの前にアイコン画像を出せるようにした

--- contentscript.js
+++ contentscript.js
@@ -2,6 +2,10 @@
  * contentscript.js
  */
 
+/*===============*/
+/* global object */
+/*===============*/
+var ssObj;
 
 /*===================*/
 /* Constant Variable */
@@ -18,83 +22,6 @@ var arrayOfHref;
 /*============*/
 
 (function() {
-    // setTimeout(function() {
-      /*
-      var links = document.getElementsByClassName("l");
-      arrayOfHref = new Array();
-      for (var i = 0; i < links.length; i++) {
-        var link = links[i];
-        arrayOfHref.push(link.href);
-        console.log(arrayOfHref);
-        link.style.color = "#12c";
-        link.style.cursor = "pointer";
-        link.style.textDecoration = "underline";
-        link.addEventListener("click", function() {
-
-          var frameSet = document.createElement("frameset");
-          frameSet.cols = "*,*";
-
-          var iframeTag = document.createElement("frame");
-          // iframeTag.style.width = document.body.scrollWidth / 2;
-          // iframeTag.style.height = document.body.scrollHeight;
-          // iframeTag.style.width = "600px";
-          // iframeTag.style.height = "800px";
-          iframeTag.id = "splitFrame";
-          iframeTag.src = location.href;
-          while(document.body.hasChildNodes()) {
-            document.body.removeChild(document.body.lastChild);
-          }
-          document.body.appendChild(iframeTag);
-      
-          var pageFrame = document.createElement("frame");
-          pageFrame.src = arrayOfHref[0];
-          // pageFrame.style.width = document.body.scrollWidth / 2;
-          // pageFrame.style.height = document.body.scrollHeight;
-
-          frameSet.appendChild(iframeTag);
-          frameSet.appendChild(pageFrame);
-          document.body.appendChild(frameSet);
-        }, false);
-        link.removeAttribute("href");
-      }
-      */
-
-/*
-      var searchResultsFrame = document.createElement("frame");
-      searchResultsFrame.id = "searchResultsFrame";
-      document.body.appendChild(searchResultsFrame);
-
-      searchResultsFrame = document.getElementById("searchResultsFrame");
-      searchResultsFrame.style.width = "600px";
-      searchResultsFrame.style.height = "800px";
-      // contentWindowだとundefinedが返ってくる
-      var searchResultsBody = searchResultsFrame.contentDocument.body;
-      var searchResultsHead = searchResultsFrame.contentDocument.head;
-      for (var i = 0; i < document.body.childNodes.length; i++) {
-        if (document.body.childNodes[i] && document.body.childNodes[i].id == "searchResultsFrame") {
-          continue;
-        }
-        var dupBody = document.body.childNodes[i].cloneNode(true);
-        searchResultsBody.appendChild(dupBody);
-      }
-      for (var i = 0; i < document.head.childNodes.length; i++) {
-        var dupHead = document.head.childNodes[i].cloneNode(true);
-        searchResultsHead.appendChild(dupHead);
-      }
-      while (document.body.hasChildNodes()) {
-        if (document.body.childNodes[i] && document.body.childNodes[i].id == "searchResultsFrame") {
-          continue;
-        }
-        document.body.removeChild(document.body.childNodes[i]);
-      }
-      while (document.head.hasChildNodes()) {
-        document.head.removeChild(document.head.lastChild);
-      }
-      */
-
-
-      
-    // }, 3000);
   
   var searchResultsFrame = document.createElement("iframe");
   searchResultsFrame.id = "searchResultsFrame";
@@ -106,19 +33,33 @@ var arrayOfHref;
   dummyBody.appendChild(searchResultsFrame);
   timerId = setInterval(function() {
     checkDomLoad(dummyBody);
-    // checkDomLoad(dummyDoc);
   }, 100);
-  // document.body.appendChild(searchResultsFrame);
 })();
 
 
 function checkDomLoad(dummyBody) {
 
-  // if (document.body) {
-  var links = document.getElementsByClassName("l");
-  if (links.length != 0) {
+  var rs = document.getElementsByClassName("r");
+  if (rs.length != 0) {
     clearInterval(timerId);
     console.log("dom loaded");
+
+    for (i = 0; i < rs.length; i++) {
+      var r = rs[i];
+      var link = r.childNodes[0];
+      var splitLink = document.createElement("a");
+      var icon = document.createElement("img");
+
+      icon.src = chrome.extension.getURL("icon.png");
+      icon.style.width = "20px";
+      icon.style.marginRight = "5px";
+      splitLink.href = link.href;
+      splitLink.appendChild(icon);
+      splitLink.className = "splitLink";
+
+      r.appendChild(splitLink);
+      r.appendChild(link);
+    }
 
     var div = document.createElement("div");
     div.style.display = "none";
@@ -132,37 +73,21 @@ function checkDomLoad(dummyBody) {
     var searchResultsFrame = document.createElement("frame");
     searchResultsFrame.id = "searchResultsFrame";
     searchResultsFrame.src = location.href;
-    // searchResultsFrame.width = document.body.scrollWidth / 2;
-    // searchResultsFrame.height = document.body.scrollHeight;
-    // searchResultsFrame.style.display = "none";
-
 
     var pageFrame = document.createElement("frame");
-    // pageFrame.width = document.body.scrollWidth / 2;
-    // pageFrame.height = document.body.scrollHeight;
-    // pageFrame.style.position = "absolute";
-    // pageFrame.style.top = 0;
-    // pageFrame.style.left = document.body.scrollWidth / 2 + "px";
     pageFrame.name = "pageFrame";
     pageFrame.id = "pageFrame";
-    // pageFrame.style.display = "none";
 
     frameSet.appendChild(searchResultsFrame);
     frameSet.appendChild(pageFrame);
-    // iframe.contentDocument.body.appendChild(frameSet);
-    // iframe.contentDocument.body.style.margin = 0;
     div.appendChild(frameSet);
     document.body.appendChild(div);
 
     setTimeout(function() {
-      var links = document.getElementsByClassName("l");
+      var links = document.getElementsByClassName("splitLink");
       arrayOfHref = new Array();
       for (var i = 0; i < links.length; i++) {
         var link = links[i];
-        // arrayOfHref.push(link.href);
-        // link.style.color = "#12c";
-        // link.style.cursor = "pointer";
-        // link.style.textDecoration = "underline";
         link.target = "pageFrame";
 
         link.addEventListener("click", function() {
@@ -220,8 +145,6 @@ function checkDomLoad(dummyBody) {
                                 "};";
           injectScript(colScrollScript);
 
-          // searchResultsFrame.contentWindow.scrollTo(0, scrollTop);
-
           var searchResultsLinks = doc.getElementsByClassName("l");
           for (var i = 0, l = searchResultsLinks.length; i < l; i++) {
             searchResultsLinks[i].target = "pageFrame";
@@ -229,17 +152,7 @@ function checkDomLoad(dummyBody) {
 
           var fieldset = doc.getElementById("gbqff");
           fieldset.style.width = "300px";
-          // var targetIFrame = document.getElementById("targetIFrame");
-          // console.log(targetIFrame);
-          // targetIFrame.style.display = "block";
-
-          // var searchResultsFrame = document.getElementById("searchResultsFrame");
-          // searchResultsFrame.style.display = "block";
-          // var pageFrame = document.getElementById("pageFrame");
-          // pageFrame.style.display = "block";
-
         }, false);
-        // link.removeAttribute("href");
       }
       console.log(arrayOfHref);
     }, 1000);
